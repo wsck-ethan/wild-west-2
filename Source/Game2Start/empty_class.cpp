080a648f32e88712533648c1dde83247b2fb448c
wsck-ethan <131898442+wsck-ethan@users.noreply.github.com>
2024-04-30 12:24:10 -0500

    exe play able know ( created a empty c++ file)

---
+++ Source/Game2Start/empty_class.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "empty_class.h"
+
+empty_class::empty_class()
+{
+}
+
+empty_class::~empty_class()
+{
+}
